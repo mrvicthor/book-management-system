56c6e77dadd455a46a773cdcd8d39fe9bf96df5d
Victor Eleanya <victoreleanya89@gmail.com>
2025-06-07 11:48:51 +0100

    feat(LottieAnimation.tsx): added animation to home page

---
+++ src/components/animations/LottieAnimation.tsx
@@ -0,0 +1,13 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const LottieAnimation = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/190463a7-752a-49b6-a866-df640b4f0c93/QVNvnz6AxO.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+
+export default LottieAnimation;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,16 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
+import LottieAnimation from "../components/animations/LottieAnimation";
 
 const Home = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <Card sx={{ maxWidth: 400, padding: 2, textAlign: "center" }}>
-        <CardContent>
-          <Typography variant="h4" component="div" gutterBottom>
-            Book Management System
-          </Typography>
-        </CardContent>
-      </Card>
-    </Box>
+    <Card sx={{ textAlign: "center", marginTop: "16px" }}>
+      <CardContent>
+        <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+          Book Management System
+        </Typography>
+        <LottieAnimation />
+      </CardContent>
+    </Card>
   );
 };
 
